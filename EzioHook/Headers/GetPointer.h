8571d077905ba32894c3e66126fc2a6aec5f18dc
Ben <starwarssonic90@gmail.com>
2023-05-15 17:50:44 -0400

    Update GetPointer to check for Read permissions (Thanks Sajid)

--- EzioHook/Headers/GetPointer.h
+++ EzioHook/Headers/GetPointer.h
@@ -1,26 +1,32 @@
 #pragma once
- inline size_t GetPointer(size_t address, const size_t* offsets, size_t numOffsets)
- {
-     if (address == 0)
-         return 0;
+#include<windows.h>
+inline size_t GetPointer(size_t address, const size_t* offsets, size_t numOffsets)
+{
+    if (address == 0)
+        return 0;
 
-     size_t result = *(size_t*)(address);
+    size_t result = *(size_t*)(address);
 
-     if (result == 0)
-         return 0;
+    if (result == 0)
+        return 0;
 
-     if (numOffsets > 0)
-     {
-         for (size_t i = 0; i < numOffsets - 1; i++)
-         {
-             result = (size_t)((char*)(result)+offsets[i]);
-             result = *(size_t*)(result);
-             if (result == 0)
-                 return 0;
-         }
+    if (numOffsets > 0)
+    {
+        for (size_t i = 0; i < numOffsets - 1; i++)
+        {
+            result = (size_t)((char*)(result)+offsets[i]);
+            MEMORY_BASIC_INFORMATION info{};
+            VirtualQuery((void*)result, &info, sizeof(info));
+            if (info.Protect == PAGE_NOACCESS)
+                return 0;
 
-         return result + offsets[numOffsets - 1];
-     }
+            result = *(size_t*)(result);
+            if (result == 0)
+                return 0;
+        }
 
-     return result;
- }
+        return result + offsets[numOffsets - 1];
+    }
+
+    return result;
+}
