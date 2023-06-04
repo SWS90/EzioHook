17c86ad3ba04926f0b97a60cf976e949b6a5af29
Ben <starwarssonic90@gmail.com>
2023-06-04 17:32:24 -0400

    [AC2] Add Hidden Blade Poison, Pistol Bullets, Medicine, Smoke Bombs, and Throwing Knives amount editing

--- EzioHook/SourceFiles/EzioHookInventory.cpp
+++ EzioHook/SourceFiles/EzioHookInventory.cpp
@@ -9,28 +9,100 @@
 #include <EzioHookOtherWindows.h>
 void InventoryAC2()
 {
-    size_t PlayerMoneyBaseAddr = 0x022134B4;
+    size_t PlayerInventoryBaseAddr = 0x022134B4;
+
     size_t PlayerMoneyOffsets[] = { 0x20, 0x18, 0x00, 0x10, 0x58, 0xC, 0x10, 0x00, 0x10 };
-    size_t PlayerMoneyResult = GetPointer(PlayerMoneyBaseAddr, PlayerMoneyOffsets, _countof(PlayerMoneyOffsets));
+    size_t PlayerHiddenBladePoisonOffsets[] = { 0x20, 0x18, 0x00, 0x10, 0x58, 0xC, 0x10, 0x10, 0x10 };
+    size_t PlayerPistolBulletsOffsets[] = { 0x20, 0x18, 0x00, 0x10, 0x58, 0xC, 0x10, 0x14, 0x10 };
+    size_t PlayerMedicineOffsets[] = { 0x20, 0x18, 0x00, 0x10, 0x58, 0xC, 0x10, 0xC, 0x10 };
+    size_t PlayerSmokeBombsOffsets[] = { 0x20, 0x18, 0x00, 0x10, 0x58, 0xC, 0x10, 0x04, 0x10 };
+    size_t PlayerThrowingKnivesOffsets[] = { 0x20, 0x18, 0x00, 0x10, 0x58, 0xC, 0x10, 0x20, 0x1E };
+
+    size_t PlayerMoneyResult = GetPointer(PlayerInventoryBaseAddr, PlayerMoneyOffsets, _countof(PlayerMoneyOffsets));
+    size_t PlayerHiddenBladePoisonResult = GetPointer(PlayerInventoryBaseAddr, PlayerHiddenBladePoisonOffsets, _countof(PlayerHiddenBladePoisonOffsets));
+    size_t PlayerPistolBulletsResult = GetPointer(PlayerInventoryBaseAddr, PlayerPistolBulletsOffsets, _countof(PlayerPistolBulletsOffsets));
+    size_t PlayerMedicineResult = GetPointer(PlayerInventoryBaseAddr, PlayerMedicineOffsets, _countof(PlayerMedicineOffsets));
+    size_t PlayerSmokeBombsResult = GetPointer(PlayerInventoryBaseAddr, PlayerSmokeBombsOffsets, _countof(PlayerSmokeBombsOffsets));
+    size_t PlayerThrowingKnivesResult = GetPointer(PlayerInventoryBaseAddr, PlayerThrowingKnivesOffsets, _countof(PlayerThrowingKnivesOffsets));
+
+    ImGui::Checkbox("Show Adjustment Amount Config", &ShowPlayerInventoryAdjustAmountWindowAC2);
+    if (ShowPlayerInventoryAdjustAmountWindowAC2)
+    {
+        EzioHookAC2PlayerInventoryAdjustAmountWindow();
+    }
+
     if (PlayerMoneyResult == 0)
     {
-        ImGui::Checkbox("Show Adjustment Amount Config", &ShowPlayerInventoryAdjustAmountWindowAC2); 
         ImGui::BeginDisabled(true);
-        ImGui::InputInt("Florins(Money)", &EHIntCannotRead, 1, 0);
-        if (ShowPlayerInventoryAdjustAmountWindowAC2)
-        {
-            EzioHookAC2PlayerInventoryAdjustAmountWindow();
-        }
+        ImGui::InputInt("Florins(Money)", &EHIntCannotRead, 0, 0);
         ImGui::EndDisabled();
     }
+
     if (PlayerMoneyResult != 0)
     {
-        ImGui::Checkbox("Show Adjustment Amount Config", &ShowPlayerInventoryAdjustAmountWindowAC2);
         ImGui::InputInt("Florins(Money)", (int*)PlayerMoneyResult, EHPlayerMoneyAdjustAmount, EHPlayerMoneyAdjustAmountFast);
         *((int*)PlayerMoneyResult) = std::clamp(*(int*)PlayerMoneyResult, 20, 2000000000);
-        if (ShowPlayerInventoryAdjustAmountWindowAC2)
-        {
-            EzioHookAC2PlayerInventoryAdjustAmountWindow();
-        }
+    }
+
+    if (PlayerHiddenBladePoisonResult == 0)
+    {
+        ImGui::BeginDisabled(true);
+        ImGui::SliderInt("Hidden Blade Poison", &EHIntCannotRead, 0, 0);
+        ImGui::EndDisabled();
+    }
+
+    if (PlayerHiddenBladePoisonResult != 0)
+    {
+        ImGui::SliderInt("Hidden Blade Poison", (int*)PlayerHiddenBladePoisonResult, 0, 99, NULL, ImGuiSliderFlags_AlwaysClamp);
+    }
+
+    if (PlayerPistolBulletsResult == 0)
+    {
+        ImGui::BeginDisabled(true);
+        ImGui::SliderInt("Pistol Bullets", &EHIntCannotRead, 0, 0);
+        ImGui::EndDisabled();
+    }
+
+    if (PlayerPistolBulletsResult != 0)
+    {
+        ImGui::SliderInt("Pistol Bullets", (int*)PlayerPistolBulletsResult, 0, 99, NULL, ImGuiSliderFlags_AlwaysClamp);
+    }
+
+    if (PlayerMedicineResult == 0)
+    {
+        ImGui::BeginDisabled(true);
+        ImGui::SliderInt("Medicine", &EHIntCannotRead, 0, 0);
+        ImGui::EndDisabled();
+    }
+
+    if (PlayerMedicineResult != 0)
+    {
+        ImGui::SliderInt("Medicine", (int*)PlayerMedicineResult, 0, 99, NULL, ImGuiSliderFlags_AlwaysClamp);
+    }
+    
+    if (PlayerSmokeBombsResult == 0)
+    {
+        ImGui::BeginDisabled(true);
+        ImGui::SliderInt("Smoke Bombs", &EHIntCannotRead, 0, 0);
+        ImGui::EndDisabled();
+    }
+
+    if (PlayerSmokeBombsResult != 0)
+    {
+        ImGui::SliderInt("Smoke Bombs", (int*)PlayerSmokeBombsResult, 0, 99, NULL, ImGuiSliderFlags_AlwaysClamp);
+    }
+
+    if (PlayerThrowingKnivesResult == 0)
+    {
+        ImGui::BeginDisabled(true);
+        ImGui::SliderInt("Throwing Knives", &EHIntCannotRead, 0, 0);
+        ImGui::EndDisabled();
+    }
+
+    if (PlayerThrowingKnivesResult != 0)
+    {
+        const uint8_t MinThrowingKnives = 0;
+        const uint8_t MaxThrowingKnives = 15;
+        ImGui::SliderScalar("Throwing Knives", ImGuiDataType_U8, (uint8_t*)PlayerThrowingKnivesResult, &MinThrowingKnives, &MaxThrowingKnives, "%u", ImGuiSliderFlags_AlwaysClamp);
     }
 }
